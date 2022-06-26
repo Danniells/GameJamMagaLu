2bae54dbb2de18ad82e4eb39b208bcd1d6d8796a
waagreen <silvacostawagner@gmail.com>
2022-06-26 20:47:59 -0300

    trigger fix

--- Assets/Scripts/NextScreenTrigger.cs
+++ Assets/Scripts/NextScreenTrigger.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class NextScreenTrigger : MonoBehaviour
 {
+
+
     [SerializeField] GameObject player;
-    private void OnTriggerEnter2D(Collider2D other) {
+
+    private void Awake() {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
         if(other.tag == "Player") ActivateTransition();
     }
 
     private void ActivateTransition()
     {
-        player.
+        player.transform.position += Vector3.up * 3f;
         Camera.main.transform.position += Vector3.up * 10.7f;
     }
 }
